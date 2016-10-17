58a83141f7493999a7136b1723bdb48d29fc1a7f
ivanbmstu <Purat33@gmail.com>
2016-10-18 01:27:42 +0300

    add reminder service

---
+++ src/main/scala-2.11/service/ReminderService.scala
@@ -0,0 +1,73 @@
+package service
+
+import java.sql.Timestamp
+import java.time.{ZoneOffset, LocalDateTime, Period, Instant}
+import java.util.concurrent.CopyOnWriteArrayList
+
+import akka.actor.{ActorRef, Actor}
+import akka.actor.Actor.Receive
+import akka.pattern.AskableActorRef
+import com.typesafe.scalalogging.Logger
+import entities.db.MapEvent
+import service.ReminderService.Add
+import spray.json.{JsString, JsObject}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.parallel.immutable
+import scala.concurrent.duration.Duration
+
+/**
+  * Created by ivan on 17.10.16.
+  */
+class ReminderService(_fcmService: ActorRef) extends Thread{
+  private val logger = Logger("webApp")
+  private val joinService = new JoinEventService()
+  private var array = new immutable.ParVector[MapEvent]()
+  def addEvent(event: MapEvent): Unit = {
+    array = array :+ event
+    logger.debug("add event: " + event)
+    logger.debug("size: " + array.length)
+
+  }
+  def updateEvent(event: MapEvent) = {
+    logger.debug("updating " + event)
+    val index = array.indexWhere(_.id == event.id)
+    if (index != -1) {
+      array = array.updated(index, event)
+    }
+  }
+
+  override def run(): Unit = {
+    while (!Thread.interrupted()) {
+      val workArray = array.filter(_.date.getTime >= LocalDateTime.now().minusHours(1).toEpochSecond(ZoneOffset.UTC))
+      logger.debug("New incoming events: " + workArray.map(_.id))
+      workArray.map(item => (item.id.get,item.description, item.name))
+        .foreach( (i:(Int,Option[String],String)) => {
+          joinService.getTokens(i._1).onSuccess {
+            case res =>
+              logger.debug("sendind to: " + res)
+              _fcmService ! FcmService.SendMessage(
+                res.map(_.deviceToken),
+                JsObject(
+                  ("message",JsString(s"1 hour left for event id:${i._1} name:${i._3} descr:${i._2}"))
+                )
+              )
+          }
+        }
+      )
+      array = array.filterNot(item => workArray.exists(_.id.eq(item.id)))
+
+      Thread.sleep(1000 * 30) //0.5 minute
+    }
+  }
+}
+object ReminderService {
+ case class Add(event: MapEvent)
+}
+
+class ReminderServiceActor(reminderService: ReminderService) extends Actor {
+
+  override def receive: Receive = {
+    case Add(ev) =>
+      reminderService.addEvent(ev)
+  }
+}
